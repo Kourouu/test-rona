49bab104fb9270291caad7002fe3fe2ae1cb6145
Gaetano Trapani <gaetano.trapani.dev@gmail.com>
2024-05-22 14:30:41 -0400

    create reducer

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,13 @@ import { Users } from "./components/Users/Users";
 function App() {
   const queryClient = new QueryClient();
 
+  const add = (...params: number[]) => params.reduce((acc, cur) => acc + cur);
+
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
         <Users />
+        <>{console.log(`La somme des parametres vaut : ${add(1, 4, 6)}`)}</>
       </ThemeProvider>
     </QueryClientProvider>
   );
